6da2341e09e5db10f6cfdf8d99b44dd0926dd059
Monocled <47996069+Monocled@users.noreply.github.com>
2021-05-18 03:13:22 +0800

    replace const
    const {prefix , token} => const config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {prefix, token} = require('./config.json');
+const config = require('./config.json');
 
 client.once('ready', () => {
 	console.log('${client.user.tag} Ready!');
